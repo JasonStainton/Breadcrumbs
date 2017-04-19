edd3f4f25f35c4d31b8eeb6e25be370c664eb453
Jason Stainton <jason@engageinteractive.co.uk>
2017-04-19 12:01:40 +0100

    Moved views out of psr loaded src directory.

--- src/Providers/BreadcrumbsServiceProvider.php
+++ src/Providers/BreadcrumbsServiceProvider.php
@@ -9,10 +9,10 @@ class BreadcrumbsServiceProvider extends ServiceProvider
 {
     public function boot()
     {      
-        $this->loadViewsFrom(__DIR__ . '/../Views', 'breadcrumbs');
+        $this->loadViewsFrom(__DIR__ . '/../../views', 'breadcrumbs');
 
         $this->publishes([
-            __DIR__ . '/../Views' => resource_path('views/vendor/breadcrumbs'),
+            __DIR__ . '/../../views' => resource_path('views/vendor/breadcrumbs'),
         ]);
 
         Blade::directive('breadcrumbs', function($expression)
